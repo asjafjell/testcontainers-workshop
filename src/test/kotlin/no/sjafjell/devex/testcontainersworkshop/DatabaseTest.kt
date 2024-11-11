f8e019ffe857486e0da7c55d7f45cb0167ae8317
Aleksander Aas Sjåfjell <github@sjafjell.no>
2024-11-11 08:57:07 +0100

    feat: Add complete code for workshop

---
+++ src/test/kotlin/no/sjafjell/devex/testcontainersworkshop/DatabaseTest.kt
@@ -0,0 +1,32 @@
+package no.sjafjell.devex.testcontainersworkshop
+
+import java.sql.ResultSet
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatabaseTest {
+
+    data class Person(val name: String, val avdeling: String)
+
+    @Test
+    fun `insert and get person`() {
+        /*
+        TestDataSource
+            .jdbcTemplate()
+            .queryForObject(
+                "SELECT name, department FROM person WHERE id = 1",
+                { resultSet: ResultSet, currentIndex: Int -> this.mapToPerson(resultSet, currentIndex) }
+            )
+
+        println("Datasource ${TestDataSource.jdbcTemplate()} created")
+        */
+    }
+
+    fun mapToPerson(rs: ResultSet, currentIndex: Int): Person = Person(
+        name = rs.getString("name"),
+        avdeling = rs.getString("department")
+    )
+
+}
